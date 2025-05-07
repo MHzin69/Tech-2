e46cbf8440e81b339a0d534eb5306163e12394bd
MHzin69 <lohannluna@gmail.com>
2025-05-06 22:53:27 -0300

    inicio

---
+++ ex7.js
@@ -0,0 +1,10 @@
+// Crie um array chamado livros contendo 7 títulos de livros definidos
+// por você. Após declarar o array, exiba todos os elementos com suas
+// respectivas posições no formato: "Posição [índice]: título". Em
+// seguida, implemente um processo que mostre os elementos
+// restantes do array, excluindo o último item a cada iteração. Utilize o
+// método pop() para remover o último livro do array e continue
+// exibindo os livros restantes até que o array esteja vazio. Ao final,
+// exiba uma mensagem indicando que não há mais livros disponíveis.     
+
+let livros = ["Harry Potter e a Pedra Filosofal", "Harry Potter e a Câmara Secreta", "Harry Potter e o Prisioneiro de Azkaban", "Harry Potter e o Cálice de Fogo", "Harry Potter e a Ordem da Fênix", "Harry Potter e o Enigma do Príncipe", "Harry Potter e as Reliquias da Morte"]
